84e6129698d8534b0e54b100835f3a2369b81331
Tim JIANG <p90eri@gmail.com>
2016-02-12 20:37:19 +0800

    Typings and source-map-support.
    Interesting...

--- hello-emacs/js/typescript/main.ts
+++ hello-emacs/js/typescript/main.ts
@@ -1,7 +1,11 @@
+/// <reference path="typings/main.d.ts" />
 import { greeter, shout, Cat, Dog, } from "./lib";
+import sourceMapSupport = require("source-map-support");
+sourceMapSupport.install();
 
 shout(new Cat);
 shout(new Dog);
+shout(null);
 
 greeter("HAI");
 greeter(1);
