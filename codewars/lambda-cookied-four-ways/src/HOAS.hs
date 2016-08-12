2364a8d7a1a99925005a3f904da3aef753bfcad0
Overmind JIANG <p90eri@gmail.com>
2016-08-12 23:52:35 +0800

    Legion invasion!

--- codewars/lambda-cookied-four-ways/src/HOAS.hs
+++ codewars/lambda-cookied-four-ways/src/HOAS.hs
@@ -1,3 +1,33 @@
 module HOAS where
 
 import qualified Data.Map as M
+import IdInt
+import Lambda
+
+data HOAS
+  = HVar IdInt
+  | HLam (HOAS -> HOAS)
+  | HApp HOAS HOAS
+
+nf :: LC IdInt -> LC IdInt
+nf = toLC . nfh . fromLC
+
+nfh :: HOAS
+nfh = \case
+  e@(HVar _) -> e
+  HLam b -> HLam (nfh . b)
+  HApp f a ->
+    case whnf f of
+      HLam b -> nfh (b a)
+      f' -> HApp f' (nfh a)
+
+whnf :: HOAS -> HOAS
+whnf = \case
+  e@(HVar _) -> e
+  HLam b -> HLam b
+  HApp f a ->
+    case whnf f of
+      HLam b -> whnf (b a)
+      f' -> HApp f' a
+
+
