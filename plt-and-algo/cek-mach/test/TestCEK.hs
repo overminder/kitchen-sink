75477757b934d93da186c9efa84fc266f2acc8d6
Overmind Jiang <p90eri@gmail.com>
2018-07-03 17:59:34 -0700

    feat(cek-mach): moar test cases

--- plt-and-algo/cek-mach/test/TestCEK.hs
+++ plt-and-algo/cek-mach/test/TestCEK.hs
@@ -8,3 +8,23 @@ testCek = describe "CEK" $ do
   it "halts on empty kont" $ do
     let w = VInt 0
     evalSteps (EValue w, emptyEnv, emptyKont) `shouldBe` Just w
+
+  it "captures lambda env" $ do
+    let
+      lam = Lam "x" (EVar "x")
+      clo = VClo lam emptyEnv
+    evalSteps (ELam lam, emptyEnv, emptyKont) `shouldBe` Just clo
+
+  it "id x = x" $ do
+    let
+      lam = Lam "x" (EVar "x")
+      w = VInt 0
+    evalSteps (EAp (ELam lam) (EValue w), emptyEnv, emptyKont) `shouldBe` Just w
+
+  it "k x y = x" $ do
+    let
+      lam = Lam "x" (ELam (Lam "y" (EVar "x")))
+      w0 = VInt 0
+      w1 = VInt 1
+    evalSteps (ELam lam `EAp` EValue w0 `EAp` EValue w1, emptyEnv, emptyKont) `shouldBe` Just w0
+
